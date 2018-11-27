66680b86f61a3394422d7b8bd72d0408ec937330
Benoît Wallon <benoit.wallon@gmail.com>
2018-11-27 08:17:39 +0100

    Supervisor mode v1.0
    - bare memory
    - no interrupt vector
    - timer interrupt are working

--- src/kernel/arch/rv64/common/boot.c
+++ src/kernel/arch/rv64/common/boot.c
@@ -3,8 +3,6 @@
 #include "arch/riscv/encoding.h"
 #include "machine.h"
 #include "csr.h"
-#include "irq.h"
-#include "trap.h"
 
 #define MSTATUS_MPP_MASK_S 0x800 /* bit 12-11 = 01 */
 #define MSTATUS_MPIE_MASK 0x80 /* bit 7 = 1 */
@@ -21,61 +19,10 @@
 
 int main(int argc, char **argv);
 
-void initialize_mstatus()
-{
-	// potentially enable FPU, potentially enable user use of counters
-
-	// Enable supervisor use of counters
-	csr_write(scounteren, -1);
-
-	// Enable software interrupt
-	csr_write(mie, MIX_MSI);
-
-	// Disable paging (bare memory)
-	csr_write(satp, 0);
-}
-
-void delegate_traps()
-{
-		// Delegate all the supervisor interruption to the supervisor
-		uint64_t interrupts = MIX_SSI | MIX_STI | MIX_SEI;
-		// Delegate most of the exceptions to the supervisor
-		uint64_t exceptions =
-			(1U << cause_instruction_address_misaligned)	|
-			(1U << cause_breakpoint)						|
-			(1U << cause_instruction_page_fault)			|
-			(1U << cause_load_page_fault)					|
-			(1U << cause_store_page_fault)					|
-			(1U << cause_user_ecall);
-		
-		csr_write(mideleg, interrupts);
-		csr_write(medeleg, exceptions);
-}
-
-void enter_supervisor_mode()
-{
-	uint64_t pmpcfg = PMP_NAPOT | PMP_R | PMP_W | PMP_X;
-	uint64_t pmpaddr = ((uint64_t)1U << 53)-1;
-	csr_set(mstatus, MSTATUS_MPP_MASK_S);
-	csr_clear(mstatus, MSTATUS_MPIE_MASK);
-	csr_write(pmpaddr0, pmpaddr);
-	csr_write(pmpcfg0, pmpcfg);
-
-	__asm__ __volatile__ (
-			"la t0, 1f\n"
-			"csrw mepc, t0\n"
-			"mret\n"
-			"1:" ::: "t0"
-	);
-}
-
 __attribute__((noreturn)) void boot_riscv()
 {
 	char *argv[] = { "femto", NULL };
 	arch_setup();
-	initialize_mstatus();
-	delegate_traps();
-	enter_supervisor_mode();
 	exit(main(1, argv));
 	__builtin_unreachable();
 }
--- src/kernel/kernel/start.c
+++ src/kernel/kernel/start.c
@@ -8,6 +8,8 @@
 
 #include "csr.h"
 #include "stdint.h"
+#include "trap.h"
+#include "irq.h"
 
 
 void extern ctx_sw(struct cpu_state *, struct cpu_state *);
@@ -25,6 +27,54 @@ void extern ctx_sw(struct cpu_state *, struct cpu_state *);
 #define PMP_TOR 0x8
 #define PMP_NA4 0x10
 
+
+void initialize_mstatus()
+{
+	// potentially enable FPU, potentially enable user use of counters
+
+	// Enable supervisor use of counters
+	csr_write(scounteren, -1);
+
+	// Enable software interrupt
+	csr_write(mie, MIX_MSI);
+
+	// Disable paging (bare memory)
+	csr_write(satp, 0);
+}
+
+void delegate_traps()
+{
+		// Delegate all the supervisor interruption to the supervisor
+		uint64_t interrupts = MIX_SSI | MIX_STI | MIX_SEI;
+		// Delegate most of the exceptions to the supervisor
+		uint64_t exceptions =
+			(1U << cause_instruction_address_misaligned)	|
+			(1U << cause_breakpoint)						|
+			(1U << cause_instruction_page_fault)			|
+			(1U << cause_load_page_fault)					|
+			(1U << cause_store_page_fault)					|
+			(1U << cause_user_ecall);
+		
+		csr_write(mideleg, interrupts);
+		csr_write(medeleg, exceptions);
+}
+
+void enter_supervisor_mode()
+{
+	uint64_t pmpcfg = PMP_NAPOT | PMP_R | PMP_W | PMP_X;
+	uint64_t pmpaddr = ((uint64_t)1U << 53)-1;
+	csr_set(mstatus, MSTATUS_MPP_MASK_S);
+	csr_clear(mstatus, MSTATUS_MPIE_MASK);
+	csr_write(pmpaddr0, pmpaddr);
+	csr_write(pmpcfg0, pmpcfg);
+
+	__asm__ __volatile__ (
+			"la t0, 1f\n"
+			"csrw mepc, t0\n"
+			"mret\n"
+			"1:" ::: "t0"
+	);
+}
 
 
 void idle()
@@ -50,8 +100,14 @@ void idle()
 int main(int argc, char **argv)
 {
 	printf("\n= OSON Initialization =\n");
+	initialize_mstatus();
+
+	init_machine_clock();
+
+	delegate_traps();
+	enter_supervisor_mode();
 	init_process();
-	//init_machine_clock();
+
 
 	if ( (create_kernel_process(hello, "Hello", 100, (void*) 42)) == NULL)
 	{
