650b41e1132add689aa4090fcdd7e5e67254dc39
Benoît Wallon <benoit.wallon@gmail.com>
2018-11-28 19:52:03 +0100

    removed double define warnings

--- src/kernel/boot/common/boot.c
+++ src/kernel/boot/common/boot.c
@@ -7,15 +7,6 @@
 #define MSTATUS_MPP_MASK_S 0x800 /* bit 12-11 = 01 */
 #define MSTATUS_MPIE_MASK 0x80 /* bit 7 = 1 */
 
-#define PMP_NAPOT 0x18
-#define PMP_R 0x1
-#define PMP_W 0x2
-#define PMP_X 0x4
-#define PMP_A 0x18
-#define PMP_L 0x80
-#define PMP_SHIFT 0x2
-#define PMP_TOR 0x8
-#define PMP_NA4 0x10
 
 int main(int argc, char **argv);
 
