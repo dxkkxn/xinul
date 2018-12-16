27f812fd94a667afef012b3f282bef33d8bc6ddb
Benoît Wallon <benoit.wallon@gmail.com>
2018-12-16 10:26:56 +0100

    refactoring(scheduler)
    All the function of scheduler.h have been implemented
    Refactoring

--- src/kernel/include/scheduler.h
+++ src/kernel/include/scheduler.h
@@ -28,77 +28,71 @@ enum p_status {
 	NB_STATES
 };
 
-/* Initialisation de l'ordonnanceur */
+/* Scheduler initialization */
 void sched_init(void);
 
-/* Lancement d'un processus : */
-/*  - kernel : Le processus pourra s'exécuter à partir du prochain schedule() */
+/* Start of a process: */
+/*  - kernel */
 int sched_kstart(int (*run) (void *),
                  int prio,
                  const char *name,
                  void *arg);
 
-/*  - user : Le scheduler est appelé si besoin */
+/*  - user */
 int sched_ustart(const char *name,
                  unsigned long ssize,
                  int prio,
                  void *arg);
 
-/* Ordonnancement */
+/* Scheduling: select the next active process */
 void schedule(void);
 
-/* Terminaison d'un processus */
+/* End the current process with its returned value, reboot if pid = 1 */
 void sched_exit(int retval);
 
-/* Tue le processus de pid donné */
+/* Kill the process of the given pid */
 int sched_kill(int pid);
 
-/* PID du processus actif */
+/* Return the pid of the active process */
 int sched_get_active_pid(void);
 
 /*
- * Bloque un processus dans la file donnée en paramètre
- * info         : un champ d'information à stocker dans le processus
- * queue        : une file de priorité
- * new_status   : un status à donner au processus
- * error        : si non null, prend la valeur du champ error une fois
- *                le processus débloqué
- * Retourne la valeur du champ info une fois le processus débloqué
+ * Bock a process in the given queue
+ * info         : information to store in the process
+ * queue        : priority queue
+ * new_status   : ne status of the process
+ * error        : if not null, take of value of the process error field one
+ *                the process is unblocked
+ * Return the info field value
  */
 int sched_block(long info, link *queue, int new_status, int *error);
 
 /*
- * Débloque un processus précédemment bloqué avec sched_block()
- * Renvoie une valeur différente de 0 le scheduler doit
- * être appelé
+ * Unblock a previously blocked process (with sched_block)
+ * Return != 0 if the scheduler must be called
  */
 int sched_unblock(process_t *p);
 
-/* Réordonne la file de priorité des processus actifs
- * après changement de la priorité du processus p
- * passé en paramètre */
-void sched_reorder(process_t *p);
-
-/* Attend l'un des fils du processus appelant
- * pid :        le pid du fils a attendre ou -1 pour le premier venu
- * retvalp:     pointeur sur la valeur de retour de fils (variable out)
+/* Wait one of the child of the active process
+ * pid :        pid of the son of the child (or -1 for the first to end)
+ * retvalp:     pointer on the return value of the son
  */
 int sched_waitpid(int pid, int *retvalp);
 
-/* Variante non bloquante de sched_waitpid()
- * pid :        le pid du fils a attendre ou -1 pour le premier venu
- * retvalp:     pointeur sur la valeur de retour de fils (variable out)
+/* Non blocking sched_waitpid()
+ * pid :        pid of the son of the child (or -1 for the first to end)
+ * retvalp:     pointer on the return value of the son
  */
 int sched_waitpid_nohang(int pid, int *retvalp);
 
 /*
- * Change la priorité d'un processus, et le signale à l'ordonnanceur si
- * il est dans la liste des activables.
+ * Change the priority of a process and signal if to the scheduler if it is
+ * in the activable list
  */
 int sched_chprio(int pid, int newprio);
 
 /*
- * Imprime dans un buffer l'état du processus p
+ * Write buffer with the state of the process
  */
 int sched_printstatus(process_t *p, char *buffer, unsigned int size);
 
--- src/kernel/scheduler.c
+++ src/kernel/scheduler.c
@@ -43,10 +43,177 @@ static context_t dummy;
 /* Process family handling */
 #define FAMILY_QUEUE_ADD(ptr, parent)									\
 	queue_add(ptr, &parent->children, process_t, family_link, prio)
+
 #define FAMILY_QUEUE_DELETE(ptr)										\
 	queue_del(ptr, family_link)
 
-/* ==== Scheduler initialization ==== */
+/* ====      Internal functions      ==== */
+
+/* 
+ * Iterate of the process children and destroy the zombies, mark others as
+ * orphans
+ */
+static inline void sched_cleanup_children(process_t* p);
+
+/* Kill the process of make it a zomby if it has a father */
+static void sched_zombify(process_t* p);
+
+static inline void sched_cleanup_children(process_t* p)
+{
+	process_t* child;
+	while ((child = queue_out(&p->children, process_t, family_link))) {
+		if (child->status == ZOMBIE) {
+			STATUS_QUEUE_DELETE(child);
+			process_destroy(child->pid);
+		} else {
+			child->parent = NULL;
+		}
+	}
+}
+
+static void sched_zombify(process_t* p)
+{
+        process_t* parent = p->parent;
+
+        sched_cleanup_children(p); //TODO implanter cette fonction
+
+        if (parent == NULL) {
+                /* Mark the process */
+                queue_add(p, &dustbin, process_t, status_link, prio);
+                return;
+        }
+
+        /* Become a zomby and unblock the father */
+        STATUS_QUEUE_ADD(p, ZOMBIE);
+        if (parent->status == BLOCKED_ON_WSON &&
+                        (parent->info < 0 || p->pid == parent->info)) {
+                sched_unblock(parent);
+        }
+}
+
+/*
+ * Wait of son process
+ * pid		: son PID or -1
+ * retvalp	: pointer on the return value of the child or NULL
+ * nohang	: null if blocking function
+ */
+static inline int sched_waitpid_full(int pid, int *retvalp, int nohang)
+{
+        process_t *child;
+        int ret;
+
+        /* Si le processus n'existe pas */
+        if (pid == 0 || pid > NBPROC)
+                return -1;
+
+        /* Récupérer le fils et vérifier qui est le pere ! */
+        child = (pid > 0)
+                ? process_get(pid)
+                : queue_top(&active->children, process_t, family_link);
+
+        if (child == NULL || child->parent != active)
+                return -1;
+
+        /* Si le processus n'est pas zombie il faut l'attendre */
+        if (child->status != ZOMBIE) {
+                if (nohang)
+                        return -1;
+                sched_block(pid, &processes[BLOCKED_ON_WSON],
+                              BLOCKED_ON_WSON, NULL);
+        }
+
+        ret = child->pid;
+        //ret = (child->error)? 0 : child->pid;
+
+        if (retvalp != NULL)
+                *retvalp = child->info;
+
+        STATUS_QUEUE_DELETE(child);
+        FAMILY_QUEUE_DELETE(child);
+        process_destroy(child->pid);
+
+        return ret;
+}
+
+
+
+/* ====      Scheduler initialization      ==== */
+
+/* idle process */
+int idle(void *arg)
+{
+	(void) arg;
+	
+	while (1) {
+		wfi();
+	}
+
+	return 0;
+}
+
+void sched_init(){
+	// Initialize the heads of the lists
+	int i;
+	for (i = 0; i < NB_SCHED_STATES; ++i) {
+		INIT_LIST_HEAD(&processes[i]);
+	}
+	INIT_LIST_HEAD(&kernel_idle.children);
+	INIT_LIST_HEAD(&dustbin);
+
+	// Set idle's context
+	context_kernelinit(
+			&kernel_idle.context, kernel_idle.kernel_stack, idle, NULL
+	);
+
+	// Set the first process
+	STATUS_QUEUE_ADD((&kernel_idle), ACTIVABLE);
+}
+
+int sched_kstart(int (*run) (void *),
+                 int prio,
+                 const char *name,
+                 void *arg)
+{
+	int ret_pid;
+
+	if (prio < 1 || prio > MAXPRIO_KERNEL) {
+		return -1;
+	}
+
+	// Process initialization
+	process_t* p = process_create(name, 0, prio, active);
+	if (p == NULL && run == NULL) {
+		return -1;
+	}
+
+	// Process context initialization
+	context_kernelinit(&p->context, p->kernel_stack, run, arg);
+
+	// PID to return (watch out: schedule())
+	ret_pid = p->pid;
+
+	STATUS_QUEUE_ADD(p, ACTIVABLE);
+	if (active != NULL) {
+		FAMILY_QUEUE_ADD(p, active);
+	}
+	if (active == NULL || prio > active->prio) {
+		schedule();
+	}
+
+	return ret_pid;
+}
+
+int sched_ustart(const char *name,
+                 unsigned long ssize,
+                 int prio,
+                 void *arg)
+{
+	assert(0 && "Not yet implemented");
+}
+
+
+
+/* ====      Scheduling (FIFO with priorities)      ==== */
 
 void schedule(void)
 {
@@ -96,49 +263,6 @@ void schedule(void)
 	}
 }
 
-/* idle process */
-int idle(void *arg)
-{
-	(void) arg;
-	
-	while (1) {
-		wfi();
-	}
-
-	return 0;
-}
-
-void sched_init(){
-	//init_process(); //initialiser les processus et IDLE
-	//process_t kernel_idle = get_process(0); //IDLE processus
-
-	/* Initialiser les têtes de listes */
-	int i;
-	for (i = 0; i < NB_SCHED_STATES; ++i) {
-		INIT_LIST_HEAD(&processes[i]);
-	}
-	INIT_LIST_HEAD(&kernel_idle.children);
-	INIT_LIST_HEAD(&dustbin);
-
-	//TODO: restaurer le kernelinit
-	context_kernelinit(
-			&kernel_idle.context, kernel_idle.kernel_stack, idle, NULL
-	);
-
-	/* Set the first processus */
-	STATUS_QUEUE_ADD((&kernel_idle), ACTIVABLE);
-}
-
-
-
-/* ==== Scheduling (FIFO with priorities) ==== */
-static inline void sched_cleanup_children(process_t* p);
-static void sched_zombify(process_t* p);
-
-/*
- * End the current process with its returned value
- * If its init, reboot
- */
 void sched_exit(int retval)
 {
 	process_t* p = active;
@@ -163,91 +287,71 @@ void sched_exit(int retval)
 	assert(0 && "End of function sched_exit :(");
 }
 
-/* 
- * Iterate of the process children and destroy the zombies, mark others as
- * orphans
- */
-static inline void sched_cleanup_children(process_t* p)
+int sched_kill(int pid)
 {
-	process_t* child;
-	while ((child = queue_out(&p->children, process_t, family_link))) {
-		if (child->status == ZOMBIE) {
-			STATUS_QUEUE_DELETE(child);
-			process_destroy(child->pid);
-		} else {
-			child->parent = NULL;
-		}
+	process_t *p = process_get(pid);
+	
+	if (p == NULL) {
+		return -1;
 	}
-}
 
-/* Kill the process of make it a zomby if it has a father */
-static void sched_zombify(process_t* p)
-{
-        process_t* parent = p->parent;
+	// Can't kill a zombie
+	if (p->status == ZOMBIE) {
+		return -1;
+	}
 
-        sched_cleanup_children(p); //TODO implanter cette fonction
+	p->info = 0;
+	p->error = pid;
 
-        if (parent == NULL) {
-                /* Mark the process */
-                queue_add(p, &dustbin, process_t, status_link, prio);
-                return;
-        }
+	if (p != active) {
+		STATUS_QUEUE_DELETE(p);
+		sched_zombify(p);
+		return 0;
+	}
 
-        /* Become a zomby and unblock the father */
-        STATUS_QUEUE_ADD(p, ZOMBIE);
-        if (parent->status == BLOCKED_ON_WSON &&
-                        (parent->info < 0 || p->pid == parent->info)) {
-                sched_unblock(parent);
-        }
-}
-
-#if 0
-/* Fonction qui nettoye les processus finis*/
-void exit_scheduling()
-{
-	printf("exit scheduler -> donne la main\n");
-	process_t* p = active;
+	// self destruction
 	active = NULL;
-	//p->info = retval;
-	p->error = 0;
 	sched_zombify(p);
 	schedule();
-	assert(0 && "Retour de sched_exit()");
+
+	assert(0 && "Return after kamikaze sched_kill");
+	return -1;
 }
-#endif
 
-int sched_kstart(int (*run) (void *),
-                 int prio,
-                 const char *name,
-                 void *arg)
+int sched_get_active_pid(void) 
 {
-	int ret_pid;
+        return active->pid;
+}
 
-	if (prio < 1 || prio > MAXPRIO_KERNEL) {
-		return -1;
+int sched_block(long info, link* queue, int new_status, int* error)
+{
+	int retval;
+
+	assert(active != NULL
+			&& new_status < NB_STATES
+			&& new_status >= BLOCKED_ON_WSON);
+
+	active->current_queue = queue;
+	active->status = new_status;
+	active->info = info;
+	active->error = 0;
+
+	if (new_status != BLOCKED_ON_CLOCK)
+	{
+		queue_add(active, queue, process_t, status_link, prio);
+	} else {
+		queue_add(active, queue, process_t, status_link, info);
 	}
 
-	// Process initialization
-	process_t* p = process_create(name, 0, prio, active);
-	if (p == NULL && run == NULL) {
-		return -1;
+	schedule();
+
+	if (error != NULL) {
+		*error = active->error;
 	}
-
-	// Process context initialization
-	context_kernelinit(&p->context, p->kernel_stack, run, arg);
-
-	// PID to return (watch out: schedule())
-	ret_pid = p->pid;
-
-	STATUS_QUEUE_ADD(p, ACTIVABLE);
-	if (active != NULL) {
-		FAMILY_QUEUE_ADD(p, active);
-	}
-	if (active == NULL || prio > active->prio) {
-		schedule();
-	}
-
-	return ret_pid;
+	retval = active->info;
+	active->info = 0;
+	active->error = 0;
+	return retval;
 }
 
 int sched_unblock(process_t* p)
@@ -260,19 +364,27 @@ int sched_unblock(process_t* p)
 	return active != NULL && p->prio > active->prio;
 }
 
-/* ==== Utilitarian functions ==== */
-
-/* Return the pid of the active process */
-int sched_get_active_pid(void) 
+int sched_waitpid(int pid, int* retvalp)
 {
-        return active->pid;
+	return sched_waitpid_full(pid, retvalp, 0);
 }
 
-/*
- * Réordonne la file de priorité des processus actifs 
- * après changement de la priorité du processus p
- * passé en paramètre
- */
+int sched_waitpid_nohand(int pid, int* retvalp)
+{
+	process_t *p;
+
+	if (pid >= 0) {
+		return sched_waitpid_full(pid, retvalp, 1);
+	}
+
+	queue_for_each(p, &active->children, process_t, family_link) {
+		if (p->status == ZOMBIE) {
+			return sched_waitpid_full(p->pid, retvalp, 1);
+		}
+	}
+	return -1;
+}
+
 int sched_chprio(int pid, int newprio)
 {
         int oldprio;
@@ -313,9 +425,6 @@ int sched_chprio(int pid, int newprio)
 }
 
 
-/*
- * Retourne une chaîne décrivant un status
- */
 int sched_printstatus(process_t *p, char *buffer, unsigned int size)
 {
         switch (p->status) {
