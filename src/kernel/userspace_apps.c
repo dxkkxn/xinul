3d8eb0f37b0b66b7e594bcee944c325235b8f6ac
Mathieu Barbe <mathieu@kolabnow.com>
2019-01-17 16:54:29 +0100

    feature(kernel) add forget files

---
+++ src/kernel/userspace_apps.c
@@ -0,0 +1,24 @@
+/*
+ * Ensimag - Projet système
+ * Copyright (C) 2012 - Damien Dejean <dam.dejean@gmail.com>
+ *
+ * Defines structure to find user space programs.
+ */
+
+#include "string.h"
+#include "stdio.h"
+#include "userspace_apps.h"
+
+const struct uapps *find_app(const char *name)
+{
+        const struct uapps *app = symbols_table;
+
+        while (app->name != NULL) {
+                if (strcmp(name, app->name) == 0) {
+                        return app;
+                }
+                app++;
+        }
+        return NULL;
+}
+
---
+++ src/lib/include/syscall_num.h
@@ -0,0 +1,44 @@
+#ifndef ___SYSCALL_NUM_H___
+#define ___SYSCALL_NUM_H___
+
+#define SYSC_chprio             0
+#define SYSC_clock_settings     1
+#define SYSC_cons_echo          2
+#define SYSC_cons_read          3
+#define SYSC_cons_write         4
+#define SYSC_current_clock      5
+#define SYSC_exit               6
+#define SYSC_getpid             7
+#define SYSC_getprio            8
+#define SYSC_kill               9
+#define SYSC_pcount             10
+#define SYSC_pcreate            11
+#define SYSC_pdelete            12
+#define SYSC_preceive           13
+#define SYSC_preset             14
+#define SYSC_psend              15
+#define SYSC_start              16
+#define SYSC_wait_clock         17
+#define SYSC_waitpid            18
+
+#define SYSC_sleep              19
+#define SYSC_getname            20
+#define SYSC_getstatus          21
+#define SYSC_psize              22
+#define SYSC_waitpid_nohang     23
+#define SYSC_surface_info       24
+#define SYSC_surface_update     25
+#define SYSC_sleepms            26
+#define SYSC_waitmouse          27
+#define SYSC_beep               28
+#define SYSC_cons_chbuffer      29
+#define SYSC_cons_wait          30
+#define SYSC_reboot             31
+
+#define SYSC_shm_create         32
+#define SYSC_shm_acquire        33
+#define SYSC_shm_release        34
+
+#define NB_SYSCALLS             35
+
+#endif
