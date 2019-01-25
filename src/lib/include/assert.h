7eeac07ab6fd8c8da30253d702c649b60dc26089
Mathieu Barbe <mathieu@kolabnow.com>
2019-01-25 23:09:32 +0100

    refactor (lib) : refactor assert and clean debug

--- src/lib/include/assert.h
+++ src/lib/include/assert.h
@@ -1,45 +1,12 @@
-/*
- * debug.h
- *
- * Copyright (C) 2003 Simon Nieuviarts
- *
- * Functions to help writing on the screen.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version. 
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
+#ifndef __ASSERT_H__
+#define __ASSERT_H__
 
 #include "stdio.h"
 
-extern void panic(const char *fmt, ...) __attribute__((noreturn, format (printf, 1, 2)));
 
-#define BUG() do { panic(__FILE__":%u: BUG !\n", __LINE__); } while (0)
+#define die(str, ...) ({ \
+  printf("%s:%d: " str "\n", __FILE__, __LINE__, ##__VA_ARGS__); exit(-1); })
 
-#ifdef NDEBUG
-
-#define assert(expr) ((void)0)
-
-#else
-
-#define assert(expr) \
-	((void)((expr) ? 0 : \
-		(panic(__FILE__":%u: failed assertion `"#expr"'\n", \
-			__LINE__), 0)))
-
-#endif
+#define assert(x) ({ if (!(x)) die("assertion failed: %s", #x); })
 
 #endif
