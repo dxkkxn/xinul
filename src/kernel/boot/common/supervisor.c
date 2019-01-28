221383f18b9b517abd5472b63af8993c72f0b96f
Mathieu Barbe <mathieu@kolabnow.com>
2019-01-28 13:44:37 +0100

    comment (zybo) : add bug comment

--- src/kernel/boot/common/supervisor.c
+++ src/kernel/boot/common/supervisor.c
@@ -10,20 +10,20 @@ extern void strap_entry();
 
 void delegate_traps()
 {
-		// Delegate all the supervisor interruption to the supervisor
-		uint64_t interrupts = MIP_SSIP | MIP_STIP | MIP_SEIP;
-		// Delegate most of the exceptions to the supervisor
-		uint64_t exceptions =
-			(1U << cause_instruction_address_misaligned)	|
-			(1U << cause_breakpoint)						|
-			(1U << cause_illegal_instruction)				|
-			(1U << cause_instruction_page_fault)			|
-			(1U << cause_load_page_fault)					|
-			(1U << cause_store_page_fault)					|
+	// Delegate all the supervisor interruption to the supervisor
+	uint64_t interrupts = MIP_SSIP | MIP_STIP | MIP_SEIP;
+	// Delegate most of the exceptions to the supervisor
+	uint64_t exceptions =
+			(1U << cause_instruction_address_misaligned) |
+			(1U << cause_breakpoint) |
+			(1U << cause_illegal_instruction) |
+			(1U << cause_instruction_page_fault) |
+			(1U << cause_load_page_fault) |
+			(1U << cause_store_page_fault) |
 			(1U << cause_user_ecall);
 
-		csr_write(mideleg, interrupts);
-		csr_write(medeleg, exceptions);
+	csr_write(mideleg, interrupts);
+	csr_write(medeleg, exceptions);
 }
 
 void enter_supervisor_mode()
@@ -32,21 +32,26 @@ void enter_supervisor_mode()
 	// Disable paging (bare memory)
 	csr_write(satp, 0);
 
-	// set the previous context in mstatus
-
 	// Set the trap vector (direct mode)
-	csr_write(stvec, (unsigned long)strap_entry | 0UL);
+	csr_write(stvec, (unsigned long) strap_entry | 0UL);
 	// Enable timer interrupt at a machine level to be able to catch it in
 	csr_set(mie, MIP_MTIP);
+
+	// todo : bug ici :
+	// si csrw mstatus/mpp avant mie,
+	// csrw mie modifi mstatus mpp à 0
+	// on neva en mode user plutôt quand mode supervisor
+	// set the previous context in mstatus
+
 	csr_set(mstatus, MSTATUS_MPP & MSTATUS_MPP_S);
 	uint64_t status = csr_read(mstatus);
-	printf("status :%lx\n",status);
+	printf("status :%lx\n", status);
 
 	__asm__ __volatile__ (
-			"la t0, 1f\n"
-			"csrw mepc, t0\n"
-			"mret\n"
-			"1:" ::: "t0"
+	"la t0, 1f\n"
+	"csrw mepc, t0\n"
+	"mret\n"
+	"1:":: : "t0"
 	);
 
 }
