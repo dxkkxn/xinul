8fe8c6d73488083e205775723053fd417232045e
Benoît Wallon <benoit.wallon@gmail.com>
2019-01-29 10:43:46 +0100

    sifive target updated

--- src/kernel/boot/qemu-sifive_u/setup.c
+++ src/kernel/boot/qemu-sifive_u/setup.c
@@ -1,7 +1,10 @@
 // See LICENSE for license details.
 
-#include "device.h"
+#include "stdint.h"
 #include "auxval.h"
+#include "device.h"
+#include "encoding.h"
+#include "csr.h"
 
 auxval_t __auxv[] = {
     { UART0_CLOCK_FREQ,         32000000   },
@@ -17,4 +20,14 @@ void arch_setup()
     register_poweroff(&poweroff_sifive_test);
 	register_clint(&sifive_clint);
 	register_plic(&sifive_plic);
+
+	// Enable supervisor use of counters
+	csr_write(scounteren, -1);
+
+	// PPM configuration
+	uint64_t pmpcfg = PMP_NAPOT | PMP_R | PMP_W | PMP_X;
+	uint64_t pmpaddr = ((uint64_t) 1U << 53) - 1;
+	// allow access to all of the memory for everyone
+	csr_write(pmpaddr0, pmpaddr);
+	csr_write(pmpcfg0, pmpcfg);
 }
