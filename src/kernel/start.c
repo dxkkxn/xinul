f1907c93858dfd6016e16aceb27c715fdfe54395
Benoît Wallon <benoit.wallon@gmail.com>
2019-01-30 10:41:53 +0100

    kbd handler does nothing, #if 0 instead of comment in main

--- src/kernel/start.c
+++ src/kernel/start.c
@@ -20,12 +20,17 @@ int64_t launcher(void *arg)
 		assert(0 && "Unable to run app autotest\n");
 	}
 
+#if 0
+	if (sched_ustart("autotest", 1024, 11, (void *) 0) < 0) {
+		assert(0 && "Unable to run app autotest\n");
+	}
 
-//	printf("Nombre de frame disponible avant app hello : %d\n", hmm_frame_count());
-//	if (sched_ustart("hello", 1024, 11, (void *) 0) < 0) {
-//		assert(0 && "Unable to create programme hello userde test ");
-//	}
+	printf("Nombre de frame disponible avant app hello : %d\n", hmm_frame_count());
+	if (sched_ustart("hello", 1024, 11, (void *) 0) < 0) {
+		assert(0 && "Unable to create programme hello userde test ");
+	}
 //	printf("Nombre de frame disponible aprèes app hello : %d\n", hmm_frame_count());
+#endif
 	return 0;
 }
 
