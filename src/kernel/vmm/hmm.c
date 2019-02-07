2d765cc025aa6ac04785042d5b222e500a3f6887
Mathieu Barbe <mathieu@kolabnow.com>
2019-02-07 23:08:14 +0100

    feat (hmm) get my implem of hmm without intensive usage of HEAP

--- src/kernel/vmm/hmm.c
+++ src/kernel/vmm/hmm.c
@@ -14,91 +14,72 @@
 #include "string.h"
 #include "machine.h"
 
+
+struct frame_elem {
+	int priority;
+	link linker;
+};
+
+static void *frame_start;
+static void * frame_end;
+
 #define FRAME_SIZE_MASK         0xFFFFF000
 #define FRAME_SIZE              0x1000
-
-/** Describes a physical frame */
-struct frame_descriptor {
-	void *address;
-	unsigned int order;
-	link list;
-};
 
 /** Free frames list */
 static LIST_HEAD(free_frames);
 /** Free frames count */
 static unsigned int frame_count;
-/** Frame limits */
-static unsigned int frame_start;
-static unsigned int frame_end;
 
-/** Convenience list utils */
-#define FREE_LIST_ADD(element)                                                 \
-    queue_add((element), &free_frames, struct frame_descriptor, list, order)
-#define FREE_LIST_DEL(element)                                                 \
-    queue_del((element), list)
-#define FREE_LIST_GET()                                                        \
-    queue_out(&free_frames, struct frame_descriptor, list)
 
-void hmm_init(void *start, void *end) {
-	char *current;
-	assert(start != NULL && end != NULL);
-	assert(((unsigned long) start & FRAME_SIZE_MASK) == (unsigned long) start);
-	assert(((unsigned long) end & FRAME_SIZE_MASK) == (unsigned long) end);
-	assert((unsigned long) start < (unsigned long) end);
-
-	frame_count = 0;
-	for (current = (char *) end - 0x1000;
-		 current >= (char *) start;
-		 current -= 0x1000) {
-		struct frame_descriptor *local;
-
-		local = mem_alloc(sizeof(struct frame_descriptor));
-		assert(local != NULL);
-		local->address = current;
-		local->order = (unsigned long) current;
-		FREE_LIST_ADD(local);
-		frame_count++;
-	}
-	frame_start = (unsigned long) start;
-	frame_end = (unsigned long) end;
+void *hmm_frame_retain(void)
+{
+	if (frame_count == 0)
+		assert(0 && "No frame available");
+	void *f = queue_out(&free_frames,
+	struct frame_elem, linker);
+	memset(f, 0, FRAME_SIZE);
+	frame_count--;
+	return f;
 }
 
-void *hmm_frame_retain(void) {
-	void *frame = NULL;
-	struct frame_descriptor *fd;
-
-	fd = FREE_LIST_GET();
-	if (fd != NULL) {
-		frame = fd->address;
-		mem_free(fd, sizeof(*fd));
-		frame_count--;
-	}
-	memset(frame, 0, 4096);
-	return frame;
-}
-
-void hmm_frame_release(void *frame) {
-	struct frame_descriptor *local;
-
+void hmm_frame_release(void *frame)
+{
 	assert(frame != NULL);
 	assert(((unsigned long) frame & FRAME_SIZE_MASK) == (unsigned long) frame);
 
-	local = mem_alloc(sizeof(*local));
-	assert(local != NULL);
-	local->address = frame;
-	FREE_LIST_ADD(local);
+	// Il faut mêtre à zéro tout les champ de la struct
+	memset(frame, 0, sizeof(struct frame_elem));
+	queue_add(frame, &free_frames,
+	struct frame_elem, linker, priority);
 	frame_count++;
 }
 
-unsigned int hmm_frame_count(void) {
+unsigned int hmm_frame_count(void)
+{
 	return frame_count;
 }
 
-unsigned int hmm_free_memory(void) {
+unsigned int hmm_free_memory(void)
+{
 	return frame_count * FRAME_SIZE;
 }
 
+void hmm_init(void *start, void *end)
+{
+	assert(((unsigned long) end & FRAME_SIZE_MASK) == (unsigned long) end);
+	assert((unsigned long) start < (unsigned long) end);
+	frame_count = 0;
+
+	for (void * frame_start = start; frame_start < end; frame_start += FRAME_SIZE) {
+		hmm_frame_release(frame_start);
+	}
+	frame_start = start;
+	frame_end = end;
+}
+
+
+
 int hmm_is_one_of_ours(void *frame) {
-	return ((frame_start <= (unsigned long) frame) && ((unsigned long) frame < frame_end));
+	return (((uint64_t)frame_start <= (uint64_t) frame) && ((uint64_t) frame < (uint64_t)frame_end));
 }
