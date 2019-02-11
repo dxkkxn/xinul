5d58825c0909198d08d46dbd263ee581f2f4b9df
Benoît Wallon <benoit.wallon@gmail.com>
2019-02-11 14:31:48 +0100

    demo

--- src/user/demo/demo.c
+++ src/user/demo/demo.c
@@ -9,29 +9,30 @@
 int main(void)
 {
 	int64_t nombre;
-	int prio_anne = 100;
+	int prio_alice = 100;
 	int prio_bob = 101;
 	char buf[6];
-	puts("Programme de démonstration OSON sur RISC-V");
+	puts("# Programme de démonstration OSON sur RISC-V #\n");
 
 	void* msg = shm_create("demo");
 	assert(msg != 0);
 
-	printf("Père : Je suis le pid %d et ma prio est %d\n", getpid(), getprio(getpid()));
-	puts("Père : Je vais créer deux fils Anne et Bob, et je vais attendre qu'il me donne un nombre à partir d'une graine");
-	printf("Père : Donnez moi une graine : ");
+	printf("[Père] Je suis le pid %d et ma priorité est %d\n", getpid(), getprio(getpid()));
+	puts("[Père] Je vais créer deux fils Alice et Bob, et je vais attendre qu'il me donne un nombre à partir d'une graine");
+	printf("[Père] Donnez moi une graine : ");
 	cons_read(buf, 5);
 	int64_t graine = strtol(buf, NULL, 10);
 
-	int pid = start("demo_anne", 4000, prio_anne, (void*)graine);
-	printf("Père : Ma fille anne a été créée avec le pid %d avec la prio %d et la graine %ld en argument\n", pid, getprio(pid), graine);
+	int pid = start("demo_alice", 4000, prio_alice, (void*)graine);
+	printf("[Père] Ma fille Alice a été créée avec la graine %ld en argument et pid=%d, priorité=%d\n", graine, pid, getprio(pid));
 
 	int pid2 = start("demo_bob", 4000, prio_bob, NULL);
-	printf("Père : Mon fils Bob a été créée avec le pid %d avec la prio %d\n", pid2, getprio(pid2));
+	printf("[Père] Mon fils Bob a été créée avec pid=%d et priorité=%d\n", pid2, getprio(pid2));
 
-	puts("Père : Je vais attendre que Bob est terminé et me donne sa réponse");
+	puts("[Père] Je vais attendre que Bob est terminé et me donne sa réponse\n");
 	waitpid(pid2, &nombre);
-	printf("Père : Bob m'a retourné son nombre %ld\n", nombre);
+	printf("[Père] Bob m'a retourné son nombre %ld\n", nombre);
+	printf("[Père] Fin\n");
 
 
 	return 0;
--- src/user/demo_bob/demo_bob.c
+++ src/user/demo_bob/demo_bob.c
@@ -10,17 +10,18 @@ int main(void * arg)
 	int dur = 5;
 	volatile int * msg = shm_acquire("demo");
 
-	printf("Bob : Bonjour, je vais attendre que ma soeure Anne me donne son nombre\n");
+	printf("\t\t[Bob] Bonjour, je vais attendre que ma soeur Alice me donne son nombre\n");
 	while(*msg == 0) {
-		printf("Bob : J'attend Anne encore %d secondes\n", dur);
+		printf("\t\t[Bob] J'attends Alice encore %d secondes\n", dur);
 		sleep(dur);
 	}
-	printf("Bob : J'ai eu le message de Anne qui m'a envoyée %d comme nombre\n", *msg);
-	printf("Bob : Combien voulez vous que j'ajoute à ce nombre : ");
+	printf("\t\t[Bob] J'ai eu le message d'Alice qui m'a envoyée %d comme nombre\n", *msg);
+	printf("\t\t[Bob] Combien voulez vous que j'ajoute à ce nombre : ");
 	cons_read(buf, 5);
 	int somme = strtol(buf, NULL,10);
 	int resultat = *msg + somme;
-	printf("Bob je fais donc %d + %d = %d et je le dis à mon père par valeur de retour\n", *msg, somme, resultat);
+	printf("\t\t[Bob] Je fais donc %d + %d = %d et je le dis à mon père par valeur de retour\n", *msg, somme, resultat);
+	printf("\t\t[Bob] Fin\n");
 
 	return resultat;
 
