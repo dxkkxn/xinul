06a80eb7a567dca45b0cf99c7425420dc0e9f5d2
Mathieu Barbe <mathieu@kolabnow.com>
2019-02-11 14:06:24 +0100

    demo WiP

--- src/user/demo_anne/demo_anne.c
+++ src/user/demo_anne/demo_anne.c
@@ -1,19 +1,26 @@
 #include "stdio.h"
+#include "assert.h"
+#include "stdint.h"
+#include "stdlib.h"
 #include "syscall.h"
 
 
-int main(void)
+int main(void *arg)
 {
-	int64_t nombre;
-	puts("Programme de démonstration OSON sur RISC-V");
-	puts("Je vais créer deux fils Anne et Bob, et je vais attendre qu'il me donne un nombre");
-	int pid = start("demo_anne", 4000, 128, NULL);
-	printf("Ma fille anne a été créée avec le pid %d\n", pid);
-	int pid2 = start("demo_bob", 4000, 128, NULL);
-	waitpid(pid2, &nombre);
-	printf("Bob m'a répondu %ld\n", nombre);
+	int64_t nombre = (int64_t) arg;
+	char buf[10];
 
 
+	printf("Anne : Salut, je suis Anne et mon père m'a donné le chiffre %ld\nPar combien voulez-vous que je le multiplie?\n",
+		   nombre);
+	cons_read(buf, 5);
+	int facteur = strtol(buf, NULL, 10);
+	int resultat = nombre * facteur;
+	printf("je fais donc %ld x %d = %d et je le dis à mon frère par mémoire partagée\n", nombre, facteur, resultat);
+	volatile int *msg = shm_acquire("demo");
+assert(msg != 0);
+	*msg = resultat;
+	puts("Anne : fin");
 	return 0;
 
 }
--- src/user/demo_bob/demo_bob.c
+++ src/user/demo_bob/demo_bob.c
@@ -6,24 +6,22 @@
 
 int main(void * arg)
 {
-	int64_t nombre = (int64_t) arg;
 	char buf[10];
-	printf("Je suis Anne et mon père m'a donné le chiffre %ld\nPar combien voulez-vous que je le multiplie?\n", nombre);
+	int dur = 5;
+	volatile int * msg = shm_acquire("demo");
+
+	printf("Bob : Bonjour, je vais attendre que ma soeure Anne me donne son nombre\n");
+	while(*msg == 0) {
+		printf("Bob : J'attend Anne encore %d secondes\n", dur);
+		sleep(dur);
+	}
+	printf("Bob : J'ai eu le message de Anne qui m'a envoyée %d comme nombre\n", *msg);
+	printf("Bob : Combien voulez vous que j'ajoute à ce nombre : ");
 	cons_read(buf, 5);
-	int facteur = strtol(buf, NULL,10);
-	int resultat = nombre * facteur;
-	printf("je fais donc %ld x %d = %d et je le dis à mon frère par mémoire partagée\n", nombre, facteur, resultat);
+	int somme = strtol(buf, NULL,10);
+	int resultat = *msg + somme;
+	printf("Bob je fais donc %d + %d = %d et je le dis à mon père par valeur de retour\n", *msg, somme, resultat);
 
-
-	puts("Programme de démonstration OSON sur RISC-V");
-	puts("Je vais créer deux fils Anne et Bob, et je vais attendre qu'il me donne un nombre");
-	int pid = start("demo_anne", 4000, 128, NULL);
-	printf("Ma fille anne a été créée avec le pid %d\n", pid);
-	int pid2 = start("demo_bob", 4000, 128, NULL);
-	waitpid(pid2, &nombre);
-	printf("Bob m'a répondu %ld\n", nombre);
-
-
-	return 0;
+	return resultat;
 
 }
