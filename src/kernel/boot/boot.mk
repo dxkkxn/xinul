e80b64569c6377a67a93de828e130ff3cce03f79
Mathieu Barbe <mathieu@kolabnow.com>
2019-02-12 22:48:11 +0100

    refactor (boot crt) remove crt.S in machine directories. Remove semihost machine

--- src/kernel/boot/boot.mk
+++ src/kernel/boot/boot.mk
@@ -1,5 +1,12 @@
+#
+# Projet PCSEA RISC-V
+#
+# Mathieu Barbe <mathieu@kolabnow.com> - 2019
+#
+# See license for license details.
+#
 
 BOOT_OBJS = \
 	boot/common/boot.o \
-	boot/$(MACHINE)/crt.o \
+	boot/common/crtm.o \
 	boot/$(MACHINE)/setup.o \
