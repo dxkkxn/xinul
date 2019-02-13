876b3635ce770caa761a3e25bbf7946de82c77f5
Mathieu Barbe <mathieu@kolabnow.com>
2019-02-13 08:41:28 +0100

    style (start) clean start.c

--- src/kernel/start.c
+++ src/kernel/start.c
@@ -1,3 +1,12 @@
+/*
+ * Projet PCSEA RISC-V
+ *
+ * Benoît Wallon <benoit.wallon@grenoble-inp.org> - 2019
+ * Mathieu Barbe <mathieu@kolabnow.com> - 2019
+ *
+ * See license for license details.
+ */
+
 #include "stdio.h"
 #include "stddef.h"
 #include "stdlib.h"
@@ -10,47 +19,30 @@
 #include "vmm.h"
 #include "hmm.h"
 #include "shm.h"
-#include "mapper.h"
 #include "syscall.h"
 #include "timer.h"
 #include "kbd.h"
 
+// Programme par défaut en fonction de la machine, cf kernel/bot/$(MACHINE)/setup.c
 char default_program[20];
 
 int64_t launcher(void *arg)
 {
 	int pid;
-
 	printf("Launcher start.\n");
 	if ((pid = sched_ustart(default_program, 1024, 11, (void *) 0)) < 0) {
-		assert(0 && "Unable to run app autotest\n");
+		assert(0 && "Unable to run default machine app\n");
 	}
-	sched_waitpid(pid, NULL);
-
-#if 0
-	if ((pid = sched_ustart("autotest", 1024, 11, (void *) 0)) < 0) {
-		assert(0 && "Unable to run app autotest\n");
-	}
-	sched_waitpid(pid, NULL);
-
-	printf("Nombre de frame disponible avant app hello : %d\n", hmm_frame_count());
-	if ((pid = sched_ustart("hello", 1024, 11, (void *) 0)) < 0) {
-		assert(0 && "Unable to create programme hello userde test ");
-	}
-	sched_waitpid(pid, NULL);
-//	printf("Nombre de frame disponible aprèes app hello : %d\n", hmm_frame_count());
-#endif
-	return 0;
+	return sched_waitpid(pid, NULL);
 }
 
 int64_t kernel_tests(void *arg)
 {
-	printf("Tests kernel\n");
+	puts("Tests kernel");
 	int pid = sched_kstart(test0, 10, "Test 0", 0);
 	sched_waitpid(pid, NULL);
 	pid = sched_kstart(test1, 10, "test1", 0);
 	sched_waitpid(pid, NULL);
-
 	puts("Kernel tests done.");
 	return 0;
 }
@@ -70,13 +62,9 @@ int kernel_start()
 	printf("Hardware memory manager initialization...");
 	extern char _free_memory_start[];
 	extern char _memory_end[];
-	extern char _bss_start[];
-	extern char _bss_end[];
-	(void) _bss_end;
-	(void) _bss_start;
 	hmm_init(_free_memory_start, _memory_end);
 	printf("\r\t\t\t\t\t\t\t[OK]\n");
-	printf("Number of frames %d = %d ko\n", hmm_frame_count(), hmm_free_memory() >> 10);
+	printf("\tNumber of frames %d = %d ko\n", hmm_frame_count(), hmm_free_memory() >> 10);
 
 	printf("Clock interruptions...");
 	clock_init();
@@ -84,7 +72,6 @@ int kernel_start()
 
 	printf("Virtual memory manager initialization...");
 	init_virtual_memory();
-
 	if (is_virtual_memory_enable()) {
 		printf("\r\t\t\t\t\t\t\t[OK]\n");
 
