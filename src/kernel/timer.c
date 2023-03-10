f9517fa835ac8d4ef12143270e4806caef1e5c6c
Youssef Benjelloun <ybkstudies@gmail.com>
2023-03-10 10:39:51 +0100

    counter inc

--- src/kernel/timer.c
+++ src/kernel/timer.c
@@ -45,16 +45,17 @@ void handle_mtimer_interrupt()
 {
 	printf("Tic machine \n");
 	set_machine_timer_interrupt(TIC_PER); // this fills the role of ack
-	tic++;
-    return ;
+	counter++;
+  return ;
 }
 
 void handle_stimer_interrupt()
 {
-    set_supervisor_interrupts(false);
+  set_supervisor_interrupts(false);
+	counter++;
+  /* printf("current interrupts = %lu\n", counter); */
 	// printf("\nTic supervisor %d \n", tic);
 	set_supervisor_timer_interrupt(TIC_PER); 
 	scheduler();
-	tic++;
-    return;
+  return;
 }
