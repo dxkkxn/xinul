08700b848d9d462e0f659b2b3f125a13abd91c3a
Youssef Benjelloun <ybkstudies@gmail.com>
2023-03-23 20:46:24 +0100

    stdlib.h: secmalloc added

--- src/lib/include/stdlib.h
+++ src/lib/include/stdlib.h
@@ -5,6 +5,14 @@
 __attribute__((noreturn)) void abort(void);
 __attribute__((noreturn)) void exit(int status);
 
+/*
+* @brief this macro is meant to be used in function witch returns -1 on failure
+*/
+#define secmalloc(p, n)                                                        \
+  p = malloc(n);                                                               \
+  if (p == NULL)                                                               \
+  return -1
+
 void* malloc(size_t size);
 void* calloc(size_t n_elements, size_t element_size);
 void* memalign(size_t alignment, size_t size);
