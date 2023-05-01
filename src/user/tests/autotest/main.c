dd176e69d9c1deac81c3e38210d8c27d28f973ae
Youssef Benjelloun <ybkstudies@gmail.com>
2023-05-01 21:23:50 +0200

    autotest/main.c: improving tests
    making main return 1 if a tests failed
    added report to see which test failed and simplified the code

--- src/user/tests/autotest/main.c
+++ src/user/tests/autotest/main.c
@@ -5,10 +5,7 @@
 
 #include "sysapi.h"
 
-#define TESTS_NUMBER 22
-
-const char *tests[TESTS_NUMBER] = {
-
+const char *tests[] = {
         "test0",
         "test1",
         "test2",
@@ -18,9 +15,9 @@ const char *tests[TESTS_NUMBER] = {
         "test7", //takes too long
         "test8",
         "test10",
-        "test11", 
+        "test11",
         "test12",
-        "test13",  
+        "test13",
         "test14",
         "test15",
         "test16",
@@ -32,39 +29,35 @@ const char *tests[TESTS_NUMBER] = {
         "test21",
         "test22",
         NULL
-};//*/
-
-/*
-        "test6",
-        "test9",
-        "test14",
-        "test15",
-        "test16",
-        "test17",
-        "test18",
-        "test19",
-        "test20",
-        "test21",
-        "test22",
 };
-*/
 
-int main(void)
-{
-
-        int i;
-        int pid;
-        long int ret;
-        for (i = 0; i < TESTS_NUMBER; i++) {
-                if (tests[i] == NULL){
-                        break;
-                }
-                printf("Test %s : ", tests[i]);
-                pid = start(tests[i], 4000, 128, NULL);
-                waitpid(pid, &ret);
-                assert(ret == 0);
-        }
-        printf("Auto test done.\n");
-        power_off(0);
+int test_res[sizeof(tests)/sizeof(char *)];
+void tests_report() {
+  printf("--- TESTS REPORT ---\n");
+  for (int i = 0; tests[i] != NULL; i++) {
+    printf("Test %s : ", tests[i]);
+    if (test_res[i] == 0)
+      printf("\x1B[1m\x1B[32mPASSED \x1b[0m\n");
+    else
+      printf("\x1B[1\x1B[31mFAILED \x1b[0m\n");
+  }
+  printf("end");
 }
 
+int main(void) {
+  int i;
+  int pid;
+  long int ret;
+  int has_failed = 0;
+  for (i = 0; tests[i] != NULL; i++) {
+    printf("Test %s : ", tests[i]);
+    pid = start(tests[i], 4000, 128, NULL);
+    waitpid(pid, &ret);
+    test_res[i] = (int)ret;
+    if (ret != 0 && has_failed == 0)
+      has_failed = 1;
+  }
+  printf("Auto test done.\n");
+  tests_report();
+  power_off(has_failed);
+}
