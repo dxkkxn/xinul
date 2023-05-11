22131bab3123a901a3db3af3eb0654ceb493ce69
Youssef Benjelloun <ybkstudies@gmail.com>
2023-05-11 16:16:32 +0200

    snake.c: implemented modulo surface

--- src/user/snake/snake.c
+++ src/user/snake/snake.c
@@ -63,16 +63,19 @@ void clear_terminal(int num_lines) {
 }
 
 snake_t * create_snake() {
-  printf("ok");
   node_t * queue = malloc(sizeof(node_t));
-  printf("ok");
   node_t * head = malloc(sizeof(node_t));
   queue->i = 5; queue->j = 5; queue->next = NULL;
   head->i = 5; head->j = 6; head->next = queue;
   snake_t * snake = malloc(sizeof(snake_t));
   snake->head = head;
   snake->direction = WEST;
-  return NULL;
+  return snake;
+}
+
+static int mod(int a, int b) {
+  int r = a % b;
+  return r < 0 ? r + b : r;
 }
 
 void move_snake(snake_t * snake) {
@@ -92,26 +95,30 @@ void move_snake(snake_t * snake) {
   }
   switch (snake->direction) {
     case NORTH:
-      snake->head->i -= 1;
+      snake->head->i = mod((snake->head->i - 1), LINES);
       break;
     case SOUTH:
-      snake->head->i += 1;
+      snake->head->i = mod((snake->head->i + 1), LINES);
       break;
     case EAST:
-      snake->head->j -= 1;
+      snake->head->j = mod((snake->head->j - 1), COLUMNS);
       break;
     case WEST:
-      snake->head->j += 1;
+      snake->head->j = mod((snake->head->j + 1), COLUMNS);
       break;
   }
 }
 
 int main(void) {
-  /* snake_t * snake = create_snake(); */
-  node_t queue = {5, 5, NULL};
-  node_t head = {5, 6, &queue};
-  snake_t s = {&head, WEST};
-  snake_t * snake = &s;
+  offset = 0;
+  snake_t * snake = create_snake();
+  printf("snake %p\n", snake);
+  /* int * t = malloc(1); */
+  /* (void)t; */
+  /* node_t queue = {5, 5, NULL}; */
+  /* node_t head = {5, 6, &queue}; */
+  /* snake_t s = {&head, WEST}; */
+  /* snake_t * snake = &s; */
   do {
     move_snake(snake);
     print_game(snake);
